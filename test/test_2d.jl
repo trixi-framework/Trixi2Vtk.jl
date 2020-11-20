fb2e44d953d45ae318ecaa9248d8e5ae90fdbb90
Michael Schlottke-Lakemper <michael@sloede.com>
2020-11-20 10:01:45 +0100

    Store test output files as artifacts

--- test/test_2d.jl
+++ test/test_2d.jl
@@ -9,6 +9,11 @@ include("test_trixi2vtk.jl")
 outdir = "out"
 isdir(outdir) && rm(outdir, recursive=true)
 
+# Create empty artifacts directory where all files that should be preserved will be stored
+artifacts_dir = "artifacts"
+isdir(artifacts_dir) && rm(artifacts_dir, recursive=true)
+mkdir(artifacts_dir)
+
 
 @testset "2D" begin
   run_trixi(joinpath("2d", "elixir_advection_extended.jl"), maxiters=1)
@@ -17,6 +22,13 @@ isdir(outdir) && rm(outdir, recursive=true)
     test_trixi2vtk("solution_000000.h5", outdir,
         hashes=[("solution_000000.vtu", "1ec2c93c0c9c4f4992dea54afaf2a348ece0160e"),
                 ("solution_000000_celldata.vtu", "e396c3ba63276347966d4264cf0f52d592221830")])
+
+    # Store output files as artifacts to facilitate debugging of failing tests
+    outfiles = ("solution_000000.vtu", "solution_000000_celldata.vtu")
+    testname = "uniform-mesh"
+    for outfile in outfiles
+      cp(joinpath(outdir, outfile), joinpath(artifacts_dir, testname * "-" * outfile))
+    end
   end
 
   @testset "uniform mesh with vti output" begin
@@ -24,6 +36,13 @@ isdir(outdir) && rm(outdir, recursive=true)
         hashes=[("restart_000001.vti", "664f25ab018a373774b5aad69ad3f2f5a3b21649"),
                 ("restart_000001_celldata.vtu", "e396c3ba63276347966d4264cf0f52d592221830")],
         format=:vti)
+
+    # Store output files as artifacts to facilitate debugging of failing tests
+    outfiles = ("restart_000001.vti", "restart_000001_celldata.vtu")
+    testname = "uniform-mesh-with-vti-output"
+    for outfile in outfiles
+      cp(joinpath(outdir, outfile), joinpath(artifacts_dir, testname * "-" * outfile))
+    end
   end
 end
 
