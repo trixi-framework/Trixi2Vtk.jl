e50104871caa5f9544434894f5bb209ac645078a
Michael Schlottke-Lakemper <michael@sloede.com>
2021-05-19 15:11:23 +0200

    Remove redunant methods and reuse Trixi implementation where possible

--- src/Trixi2Vtk.jl
+++ src/Trixi2Vtk.jl
@@ -6,7 +6,8 @@ using HDF5: h5open, attributes, haskey
 using ProgressMeter: @showprogress, Progress, next!
 using StaticArrays: SVector
 using TimerOutputs
-using Trixi: Trixi, transfinite_mapping, coordinates2mapping
+using Trixi: Trixi, transfinite_mapping, coordinates2mapping, polynomial_interpolation_matrix,
+             gauss_lobatto_nodes_weights
 using WriteVTK: vtk_grid, MeshCell, VTKCellTypes, vtk_save, paraview_collection
 
 # Include all top-level submodule files
--- src/interpolate.jl
+++ src/interpolate.jl
@@ -202,3 +202,60 @@ function raw2interpolated(data_gl::AbstractArray{Float64}, nodes_out)
   # Return as one 1D array for each variable
   return reshape(data_vis, n_nodes_out^ndims_ * n_elements, n_variables)
 end
+
+# Interpolate data using the given Vandermonde matrix and return interpolated values (2D version).
+function interpolate_nodes(data_in::AbstractArray{T, 3},
+                           vandermonde, n_vars) where T
+  n_nodes_out = size(vandermonde, 1)
+  data_out = zeros(eltype(data_in), n_vars, n_nodes_out, n_nodes_out)
+  interpolate_nodes!(data_out, data_in, vandermonde, n_vars)
+end
+
+function interpolate_nodes!(data_out::AbstractArray{T, 3}, data_in::AbstractArray{T, 3},
+                            vandermonde, n_vars) where T
+  n_nodes_out = size(vandermonde, 1)
+  n_nodes_in  = size(vandermonde, 2)
+
+  for j in 1:n_nodes_out
+    for i in 1:n_nodes_out
+      for v in 1:n_vars
+        acc = zero(eltype(data_out))
+        for jj in 1:n_nodes_in
+          for ii in 1:n_nodes_in
+            acc += vandermonde[i, ii] * data_in[v, ii, jj] * vandermonde[j, jj]
+          end
+        end
+        data_out[v, i, j] = acc
+      end
+    end
+  end
+
+  return data_out
+end
+
+
+# Interpolate data using the given Vandermonde matrix and return interpolated values (3D version).
+function interpolate_nodes(data_in::AbstractArray{T, 4},
+                           vandermonde, n_vars) where T
+  n_nodes_out = size(vandermonde, 1)
+  data_out = zeros(eltype(data_in), n_vars, n_nodes_out, n_nodes_out, n_nodes_out)
+  interpolate_nodes!(data_out, data_in, vandermonde, n_vars)
+end
+
+function interpolate_nodes!(data_out::AbstractArray{T, 4}, data_in::AbstractArray{T, 4},
+                            vandermonde, n_vars) where T
+  n_nodes_out = size(vandermonde, 1)
+  n_nodes_in  = size(vandermonde, 2)
+
+  for k in 1:n_nodes_out, j in 1:n_nodes_out, i in 1:n_nodes_out
+    for v in 1:n_vars
+      acc = zero(eltype(data_out))
+      for kk in 1:n_nodes_in, jj in 1:n_nodes_in, ii in 1:n_nodes_in
+        acc += vandermonde[i, ii] * vandermonde[j, jj] * vandermonde[k, kk] * data_in[v, ii, jj, kk]
+      end
+      data_out[v, i, j, k] = acc
+    end
+  end
+
+  return data_out
+end
