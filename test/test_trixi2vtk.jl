9dd56e4763a1f5ed5483ed1efa08cdeef1b5198c
Andrew Winters <andrew.ross.winters@liu.se>
2022-12-02 14:16:35 +0100

    introduce new testing functions and ability to grab reference files from external repo

--- test/test_trixi2vtk.jl
+++ test/test_trixi2vtk.jl
@@ -1,9 +1,9 @@
 using Test: @test_nowarn, @test, @testset, @test_skip
-#using Downloads: download # Also add to the Project.toml
-using SHA
+using Downloads: download
 using Trixi
 using Trixi2Vtk
 using ReadVTK
+
 
 function run_trixi(elixir; kwargs...)
   # evaluate examples in the scope of the module they're called from
@@ -11,25 +11,57 @@ function run_trixi(elixir; kwargs...)
 end
 
 
-function sha1file(filename)
-  open(filename) do f
-    bytes2hex(sha1(f))
+"""
+    get_reference_file(filename; head="main", output_directory=".", force=false)
+
+Retrieve an reference file from the
+[`Trixi2Vtk_reference_files` repository](https://github.com/trixi-framework/Trixi2Vtk_reference_files)
+at commit/branch `head` and store it in the `output_directory`. If the file already
+exists locally, do not download the file again unless `force` is true.
+Provide the `remotename` because the file structure in the `Trixi2Vtk_reference_files` repository
+is different than what will be created locally. Return the local path to the downloaded file.
+"""
+function get_reference_file(filename, remotename; head="main", output_directory=".", force=false)
+  filepath = joinpath(output_directory, filename)
+  if !isfile(filepath) || force
+    url = ("https://github.com/trixi-framework/Trixi2Vtk_reference_files/raw/"
+           * head
+           * "/reference_files/"
+           * remotename)
+    download(url, filepath)
   end
+
+  return filepath
 end
 
+# Commit in the reference file repository for which the test files will be downloaded
+# Note: The purpose of using a specific commit hash (instead of `main`) is to be able to tie a given
+#       version of Trixi2Vtk to a specific version of the test file repository. This way, also tests
+#       for older Trixi2Vtk releases should continue to work.
+TEST_REFERENCE_COMMIT = "1fb95363322c32d2391267e06671273ad2889144"
 
-function test_trixi2vtk(filenames, outdir; hashes=nothing, kwargs...)
-  @test_nowarn trixi2vtk(joinpath(outdir, filenames); output_directory=outdir, kwargs...)
-
-  if !isnothing(hashes)
-    for (filename, hash_expected) in hashes
-      hash_measured = sha1file(joinpath(outdir, filename))
-      @test_skip hash_expected == hash_measured
-    end
-  end
-end
+# Local folder to store downloaded reference files. If you change this, also adapt `../.gitignore`!
+TEST_REFERENCE_DIR = "reference_files"
 
 
+get_test_reference_file(filename, remotename) = get_reference_file(filename, remotename,
+                                                                   head=TEST_REFERENCE_COMMIT,
+                                                                   output_directory=TEST_REFERENCE_DIR)
+
+
+# Start with a clean environment: remove reference file directory if it exists
+isdir(TEST_REFERENCE_DIR) && rm(TEST_REFERENCE_DIR, recursive=true)
+mkpath(TEST_REFERENCE_DIR)
+
+
+"""
+     compare_cell_info(out_filename, ref_filename; atol=500*eps(), rtol=sqrt(eps()))
+
+Test values from the VTK file header and acutal (possibly interpolated) cell data. Uses
+`out_filename` created during testing and compares against `ref_filename` that comes
+from the
+[`Trixi2Vtk_reference_files` repository](https://github.com/trixi-framework/Trixi2Vtk_reference_files).
+"""
 function compare_cell_info(out_filename, ref_filename; atol=500*eps(), rtol=sqrt(eps()))
   ref_vtk = VTKFile(ref_filename)
   vtk = VTKFile(out_filename)
@@ -57,6 +89,14 @@ function compare_cell_info(out_filename, ref_filename; atol=500*eps(), rtol=sqrt
 end
 
 
+"""
+    compare_point_info(out_filename, ref_filename; atol=500*eps(), rtol=sqrt(eps()))
+
+Test values from the VTK file header and acutal (possibly interpolated) point data. Uses
+`out_filename` created during testing and compares against `ref_filename` that comes
+from the
+[`Trixi2Vtk_reference_files` repository](https://github.com/trixi-framework/Trixi2Vtk_reference_files).
+"""
 function compare_point_info(out_filename, ref_filename; atol=500*eps(), rtol=sqrt(eps()))
   # Load the data from both files
   ref_vtk = VTKFile(ref_filename)
@@ -106,45 +146,3 @@ macro timed_testset(name, expr)
     flush(stdout)
   end
 end
-
-#
-# TODO: Not sure where to put this or if it even works...
-#
-# """
-#     get_reference_file(filename; head="main", output_directory=".", force=false)
-# Retrieve an reference file from the
-# [`Trixi2Vtk_reference_files` repository](https://github.com/trixi-framework/Trixi2Vtk_reference_files)
-# at commit/branch `head` and store it in the `output_directory`. If the file already
-# exists locally, do not download the file again unless `force` is true. Return the local path to the
-# downloaded file.
-# """
-# function get_reference_file(filename; head="main", output_directory=".", force=false)
-#   filepath = joinpath(output_directory, filename)
-#   if !isfile(filepath) || force
-#     url = ("https://github.com/trixi-framework/Trixi2Vtk_reference_files/raw/"
-#            * head
-#            * "/reference_files/"
-#            * filename)
-#     download(url, filepath)
-#   end
-
-#   return filepath
-# end
-
-# # Commit in the reference file repository for which the test files will be downloaded
-# # Note: The purpose of using a specific commit hash (instead of `main`) is to be able to tie a given
-# #       version of Trixi2Vtk to a specific version of the test file repository. This way, also tests
-# #       for older Trixi2Vtk releases should continue to work.
-# TEST_REFERENCE_COMMIT = "update me"
-
-# # Local folder to store downloaded example files. If you change this, also adapt `../.gitignore`!
-# TEST_REFERENCE_DIR = "reference_files"
-
-
-# get_test_reference_file(filename) = get_reference_file(filename, head=TEST_REFERENCE_COMMIT,
-#                                                        output_directory=TEST_REFERENCE_DIR)
-
-
-# # Start with a clean environment: remove example file directory if it exists
-# isdir(TEST_REFERENCE_DIR) && rm(TEST_REFERENCE_DIR, recursive=true)
-# mkpath(TEST_REFERENCE_DIR)
