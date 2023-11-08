4a9417430ba29a56d7c09b7a551c2485d4608ec4
Michael Schlottke-Lakemper <michael@sloede.com>
2023-11-08 11:31:13 +0100

    Prepare test setup for CI tests triggered from upstream

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,18 @@
 using Test
 
-@testset "Trixi2Vtk" begin
-  include("test_2d.jl")
-  include("test_3d.jl")
-  include("test_manual.jl")
+# We use the `TRIXI_TEST` environment variable to determine the subset of tests to execute.
+const TRIXI_TEST = get(ENV, "TRIXI_TEST", "all")
+
+@time @testset "Trixi2Vtk" begin
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "2d" || TRIXI_TEST == "upstream"
+    include("test_2d.jl")
+  end
+
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "3d" || TRIXI_TEST == "upstream"
+    include("test_3d.jl")
+  end
+
+  @time if TRIXI_TEST == "all" || TRIXI_TEST == "manual"
+    include("test_manual.jl")
+  end
 end
