c1513c9a537422d266e0da050697ad1a4f498165
Daniel_Doehring <doehring@acom.rwth-aachen.de>
2025-09-01 22:18:29 +0200

    try

--- src/Trixi2Vtk.jl
+++ src/Trixi2Vtk.jl
@@ -1,13 +1,12 @@
 module Trixi2Vtk
 
 # Include other packages
-using EllipsisNotation
 using Glob: glob
 using HDF5: h5open, attributes, haskey
 using ProgressMeter: @showprogress, Progress, next!
 using StaticArrays: SVector
 using TimerOutputs
-using Trixi: Trixi, transfinite_mapping, coordinates2mapping, polynomial_interpolation_matrix,
+using Trixi: Trixi, transfinite_mapping, coordinates2mapping, polynomial_interpolation_matrix, (..),
              gauss_lobatto_nodes_weights, TreeMesh, StructuredMesh, UnstructuredMesh2D, P4estMesh, T8codeMesh
 using WriteVTK: vtk_grid, MeshCell, VTKCellTypes, vtk_save, paraview_collection
 
